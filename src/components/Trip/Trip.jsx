47b8ee5fc5ecd2dee599e98449df36a55d728f0a
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-10-05 12:13:23 -0500

    Add Trip component -- To display description about trip
    Co-authored-by: RaulMorales404 <r20160106@gmail.com>

---
+++ src/components/Trip/Trip.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {styles} from './TripStyle';
+
+const Contry = ({ code, name, textDirection }) => {
+  return (
+    <View style={styles.colBox}>
+      <Text style={textDirection}>{code}</Text>
+      <Text style={styles.text}>{name}</Text>
+    </View>
+  )
+}
+
+const Trip = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerTitle}>
+        <Contry code='BEG' name='Serbia' textDirection={styles.textLeft}/>
+        <Icon
+          name={'airplane'}
+          style={styles.icon}
+        />
+        <Contry code='AMS' name='Netherlands' textDirection={styles.textRight} />
+      </View>
+      <Text style={styles.dateText}>Septiember 3,2021</Text>
+    </View>
+  )
+}
+
+export default Trip;
