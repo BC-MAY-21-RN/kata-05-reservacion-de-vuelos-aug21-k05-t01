75d3c4562f93b7862a51484fba7b3c31853c8e03
Shadic78 <carlosjosue7889@gmail.com>
2021-10-12 10:21:35 -0500

    Added a object to get the months

--- src/util/bookingUtils.js
+++ src/util/bookingUtils.js
@@ -8,3 +8,18 @@ export const handleSelectorChange = (selectedItem, setSelectedOption) => {
   const [fromCountryName, fromCountryCode] = getSelectedCountryAndCode(selectedItem); 
   setSelectedOption({fromCountryName, fromCountryCode});
 };
+
+export const parsedMonths = {
+  0: 'January',
+  1: 'February',
+  3: 'March',
+  4: 'April',
+  5: 'May',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December',
+}
