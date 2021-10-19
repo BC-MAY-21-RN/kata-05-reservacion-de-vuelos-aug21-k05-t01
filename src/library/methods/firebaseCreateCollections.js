ce4a04f232c940169e23b23c23df718365dd88fe
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-10-19 10:21:21 -0500

    Extract firebaseCreateCollections
    Co-authored-by: RaulMorales404 <r20160106@gmail.com>

---
+++ src/library/methods/firebaseCreateCollections.js
@@ -0,0 +1,15 @@
+import firestore from '@react-native-firebase/firestore';
+
+export const createUserInitialData = async () => {
+    firestore()
+      .collection('bookings')
+      .doc(auth().currentUser.uid)
+      .get()
+      .then(response => {
+        if (!response.exists) {
+          firestore().collection('bookings').doc(auth().currentUser.uid).set({
+            flights: [],
+          });
+        }
+      });
+  };
